2b31cc779f5602f59fb834da1bfe2fffa5fab626
Ahmad <root@ahmadafuni.com>
2025-11-06 20:49:21 +1000

    Modify full flow for local testnet

--- full-flow.sh
+++ full-flow.sh
@@ -5,8 +5,13 @@ if [ ! -f ".env" ]; then
 	cp .env.default .env
 fi
 
+# Use env-specific beacon URL
+BEACON_URL=$(cat .env | sed -n 's/^BEACON_URL[ \t]*=[ \t]*"\(.*\)".*/\1/p')
+
+if [[ -z "$BEACON_URL" ]]; then BEACON_URL="https://ethereum-sepolia-beacon-api.publicnode.com"; fi
+
 echo -e "getting Beacon chain last slot number"
-LAST_SLOT=$(curl -X GET https://ethereum-sepolia-beacon-api.publicnode.com/eth/v2/beacon/blocks/head | jq -r '.data.message.slot')
+LAST_SLOT=$(curl -X GET "$BEACON_URL/eth/v2/beacon/blocks/head" | jq -r '.data.message.slot')
 
 echo -e "updating DO_GENESIS_SLOT value in the .env file"
 sed -i "s/^DO_GENESIS_SLOT=.*/DO_GENESIS_SLOT=\"$LAST_SLOT\"/" .env
